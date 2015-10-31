48b7705d36e393714245b5f5efafce6a162d8df1
Nam Lai <laihoangnam0210@gmail.com>
2015-10-31 18:31:55 +0900

    add Google map

--- app/app.js
+++ app/app.js
@@ -5,8 +5,16 @@ angular.module('myApp', [
   'ngRoute',
   'myApp.view1',
   'myApp.view2',
-  'myApp.version'
+  'myApp.version',
+  'uiGmapgoogle-maps'
 ]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+  config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.otherwise({redirectTo: '/view1'});
+  }])
+  .config(['uiGmapGoogleMapApiProvider', function (uiGmapGoogleMapApiProvider) {
+    uiGmapGoogleMapApiProvider.configure({
+      key: 'AIzaSyDiHVgMi9BeCd-0pBzk-GsKM-htjzj_54Y',
+      v: '3.20', //defaults to latest 3.X anyhow
+      libraries: 'weather,geometry,visualization'
+    });
+  }]);
