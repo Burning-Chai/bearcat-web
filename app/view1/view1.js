5c075ba925942671e5294b80fd65278b2438dec6
Nam Lai <namlai@Nams-MacBook-Pro.local>
2015-11-01 10:37:12 +0900

    marker & window

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -2,16 +2,67 @@
 
 angular.module('myApp.view1', ['ngRoute'])
 
-  .config(['$routeProvider', function ($routeProvider) {
+.config(['$routeProvider', function($routeProvider) {
     $routeProvider.when('/view1', {
-      templateUrl: 'view1/view1.html',
-      controller: 'View1Ctrl'
+        templateUrl: 'view1/view1.html',
+        controller: 'View1Ctrl'
     });
-  }])
+}])
 
-  .controller('View1Ctrl', ['$scope', 'uiGmapGoogleMapApi', function ($scope, uiGmapGoogleMapApi) {
-    $scope.map = { center: { latitude: 45, longitude: -73 }, zoom: 8 };
-    uiGmapGoogleMapApi.then(function (maps) {
+.controller('View1Ctrl', ['$scope', '$http', 'uiGmapGoogleMapApi', function($scope, $http, uiGmapGoogleMapApi) {
+    $scope.map = {
+        center: {
+            latitude: 45,
+            longitude: -73
+        },
+        zoom: 8,
+        events: {
+        	click: function(maps, eventName, args) {
+        		console.log(maps, eventName, args)
+        	},
+        	mouseover: function(maps, eventName, args) {
+        		console.log(maps, eventName, args)
+        	},
+        }
+    };
 
+    $scope.wifimarkers = getWifies();
+
+
+
+    uiGmapGoogleMapApi.then(function(maps) {
+    	maps.events = {
+        	click: function(maps, eventName, args) {
+        		console.log(maps, eventName, args)
+        	},
+        	mouseover: function(maps, eventName, args) {
+        		console.log(maps, eventName, args)
+        	},
+        }
     });
-  }]);
+
+
+    function getWifies() {
+
+        return [{
+            id: 0,
+            coords: {
+                latitude: 45,
+                longitude: -73
+            }
+        }, {
+            id: 1,
+            coords: {
+                latitude: 45.7,
+                longitude: -73.2
+            }
+        }, {
+            id: 2,
+            coords: {
+                latitude: 45.1,
+                longitude: -73.9
+            }
+        }]
+    }
+
+}]);
